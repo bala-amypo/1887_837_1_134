7e22277abbd4efe7925a8ab5dc315e0dfe96debe
bala-amypo <bala@amypo.in>
2025-12-22 08:03:24 +0000

    User commit

--- src/main/java/com/example/demo/PostSurgeryRecoveryTrackerApplication.java
+++ src/main/java/com/example/demo/PostSurgeryRecoveryTrackerApplication.java
@@ -1,12 +1,12 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class DemoApplicationTests {
+@SpringBootApplication
+public class PostSurgeryRecoveryTrackerApplication {
 
-    @Test
-    void contextLoads() {
+    public static void main(String[] args) {
+        SpringApplication.run(PostSurgeryRecoveryTrackerApplication.class, args);
     }
 }

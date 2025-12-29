86997e709bed7e12bc1ada3ed238114e659ce365
bala-amypo <bala@amypo.in>
2025-12-29 05:57:17 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import com.example.demo.dto.AuthRequest;
 import com.example.demo.dto.AuthResponse;
 import com.example.demo.dto.RegisterRequest;
+import com.example.demo.dto.RegisterRequest;
+
 import com.example.demo.service.AuthService;
 import org.springframework.web.bind.annotation.*;
 
